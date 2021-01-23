556aae611b4f9fd3c7fd3153d49e19ec22588c2b
arctheowl <ldevlin95@hotmail.co.uk>
2021-01-23 17:08:59 +0000

    Add files via upload
    Updating the Dashboard UI

---
+++ build/build/precache-manifest.234709420011b869c6ed4e03ac30d25e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76999a4ce651659ec7df880c09b6aba2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1975eed7e59b52cd49a",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "7a78402d1b1d55d16884",
+    "url": "/static/css/main.8c166fcb.chunk.css"
+  },
+  {
+    "revision": "b1975eed7e59b52cd49a",
+    "url": "/static/js/2.16082d0a.chunk.js"
+  },
+  {
+    "revision": "9b97ca7bb571f2541a8a5f813e2e91f6",
+    "url": "/static/js/2.16082d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c223d5965c8144cad703",
+    "url": "/static/js/3.aeec0251.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.aeec0251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04defe07c60107cc6723",
+    "url": "/static/js/4.723be222.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.723be222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ab146495b40ba184b1",
+    "url": "/static/js/5.eedb832a.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.eedb832a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a78402d1b1d55d16884",
+    "url": "/static/js/main.a81222a5.chunk.js"
+  },
+  {
+    "revision": "39ef469c1da73d1121d81d2eb5f649f8",
+    "url": "/static/js/main.a81222a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fdc4aeca2dd0047ae07",
+    "url": "/static/js/runtime-main.06faa703.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
